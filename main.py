3c0fe27698690791c57ff972fd43a369a5bdf81a
paveldavidchik <pdavidchik@gmail.com>
2020-11-08 21:58:34 +0300

    data set

---
+++ main.py
@@ -0,0 +1,32 @@
+
+from typing import Any
+
+
+class DataSet:
+    """Контейнер для хранения элементов"""
+    def __init__(self, data: dict = {}):
+        try:
+            if not isinstance(data, dict):
+                raise ValueError
+            else:
+                self.__data = data
+        except ValueError:
+            print('Ошибка присоздании экземпляра! Контейнер пуст')
+            self.__data = {}
+
+    def __setitem__(self, key, value):
+        try:
+            self.__data[key] = value
+        except TypeError:
+            print(f'{type(key)} не может быть ключом')
+
+    def __getitem__(self, item):
+        try:
+            return self.__data[item]
+        except KeyError:
+            print('Данный ключ отсутствует')
+
+
+data = DataSet()
+data['key'] = 'value'
+print(data['key2'])
